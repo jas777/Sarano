bd22fa7b030fe359f6acd9e3de4e38ac1b5f1cc8
jas777 <jas.czeszejko@wp.pl>
2021-04-06 17:31:30 +0200

    Bump gradle to 6.8.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    maven
+    `maven-publish`
 }
 
-sourceSets {
-    main {
-        resources {
-            srcDirs("src/main/resources")
-        }
-    }
-}
+//sourceSets {
+//    main {
+//        resources {
+//            srcDirs("src/main/resources")
+//        }
+//    }
+//}
 
-group = "me.user"
-version = "1.0-SNAPSHOT"
+group = "com.sarano"
+version = "v0.0.1"
 
 repositories {
     mavenCentral()
